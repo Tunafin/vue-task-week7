fb0bd85610587c29998aa5f8a2701745ef9e98a2
tunafin <tunaweichi@gmail.com>
2023-02-28 16:13:27 +0800

    feat: 新增套件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
 
-import './style.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import './assets/all.scss';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+app.use(VueAxios, axios);
 app.use(router);
 app.mount('#app');
