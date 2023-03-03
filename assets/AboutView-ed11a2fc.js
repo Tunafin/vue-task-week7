b44a4dd27266141bcf83c4b6ad7601d9b84ecd07
tunafin <tunaweichi@gmail.com>
2023-03-03 20:52:28 +0800

    deploy

---
+++ assets/AboutView-ed11a2fc.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./index-c6e7b88d.js";const c={};function n(r,s,a,_,p,f){return t(),o("h2",null,"關於")}const u=e(c,[["render",n]]);export{u as default};
---
+++ assets/CartView-cc7c670d.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index-c6e7b88d.js";const c={};function r(n,s,a,_,p,f){return o(),t("h2",null,"購物車")}const i=e(c,[["render",r]]);export{i as default};
